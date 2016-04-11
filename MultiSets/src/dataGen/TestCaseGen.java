a81fd2e775a59bd691112fddb87377105ce8448f
Michael <s3542838@student.rmit.edu.au>
2016-04-11 15:47:29 +1000

    Initial Commit

---
+++ MultiSets/src/dataGen/TestCaseGen.java
@@ -0,0 +1,14 @@
+package dataGen;
+
+public class TestCaseGen {
+
+	public TestCaseGen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
